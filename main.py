af9153cb8a3859cf97bccf78bf68b47c0fb879eb
ezmoonlow <72476396+ezmoonlow@users.noreply.github.com>
2020-11-16 10:31:22 +1000

    Update main.py
    asd

--- main.py
+++ main.py
@@ -16,19 +16,86 @@ screen = pg.display.set_mode(SIZE)
 FPS = 120
 clock = pg.time.Clock()
 
-'''
-bg_image = pg.image.load('Image/road.jpg')
-bg_image_rect = bg_image.get_rect(topleft=(0, 0))
-bg_image2_rect = bg_image.get_rect(topleft=(0, -HEIGHT))
-'''
 
 cars = [pg.image.load('Image/car1.png'), pg.image.load('Image/car2.png'),
         pg.image.load('Image/car3.png')]
 
 
+class Player(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+
+        self.image = pg.image.load('Image/car4.png')
+        self.ori_image = self.image
+        self.angle = 0
+        self.speed = 2
+        self.acceleration = 0.02
+        self.x, self.y = WIDTH // 2, HEIGHT // 2
+        self.rect = self.image.get_rect()
+        self.position = pg.math.Vector2(self.x, self.y)
+        self.velocity = pg.math.Vector2()
+
+    def update(self):
+        self.image = pg.transform.rotate(self.ori_image, self.angle)
+        self.rect = self.image.get_rect(center=self.rect.center)
+        self.position += self.velocity
+        self.rect.center = self.position
+
+        keys = pg.key.get_pressed()
+        if keys[pg.K_RIGHT]:
+            self.velocity.x = self.speed
+            self.angle -= 1
+            if self.angle < -25:
+                self.angle = -25
+        elif keys[pg.K_LEFT]:
+            self.velocity.x = -self.speed
+            self.angle += 1
+            if self.angle > 25:
+                self.angle = 25
+        else:
+            self.velocity.x = 0
+            if self.angle < 0:
+                self.angle += 1
+            elif self.angle > 0:
+                self.angle -= 1
+        if keys[pg.K_UP]:
+            self.velocity.y -= self.acceleration
+            if self.velocity.y < -self.speed:
+                self.velocity.y = -self.speed
+        elif keys[pg.K_DOWN]:
+            self.velocity.y += self.acceleration
+            if self.velocity.y > self.speed:
+                self.velocity.y = self.speed
+        else:
+            if self.velocity.y < 0:
+                self.velocity.y += self.acceleration
+                if self.velocity.y > 0:
+                    self.velocity.y = 0
+            elif self.velocity.y < 0:
+                self.velocity.y -= self.acceleration
+                if self.velocity.y < 0:
+                    self.velocity.y = 0
+        if self.rect.left < 38:
+            self.rect.left = 40
+            self.angle -= 1
+            if self.angle >= 0:
+                self.angle = 0
+        elif self.rect.right > 758:
+            self.rect.right = 756
+            self.angle += 1
+            if self.angle <= 0:
+                self.angle = 0
+
+        elif self.rect.top < 1:
+            self.rect.top = 4
+        elif self.rect.bottom > 599:
+            self.rect.bottom = 596
+
+
 class Road(pg.sprite.Sprite):
     def __init__(self, x, y):
         pg.sprite.Sprite.__init__(self)
+
 
         self.image = pg.Surface(screen.get_size())
         self.image.fill(GREY)
@@ -71,7 +138,7 @@ class Car(pg.sprite.Sprite):
                     continue
                 else:
                     list_x.append(self.rect.centerx)
-                    self.speed = random.randint(2,3)
+                    self.speed = random.randint(2, 3)
                     break
 
 
@@ -87,16 +154,13 @@ while n < 6:
         continue
     else:
         list_x.append(x)
-        all_sprite.add(Car(x, -cars[0].get_height(),cars[n] if n < len(cars) else random.choice(cars)))
+        all_sprite.add(Car(x, -cars[0].get_height(), cars[n] if n < len(cars) else random.choice(cars)))
         n += 1
 
 
-'''
-car1 = Car()
-car1_image = car1.image
-car1_w, car1_h = car1.image.get_width(), car1.image.get_height()
-car1.x, car1.y = (WIDTH - car1_w) // 2, (HEIGHT - car1_h) // 2
-'''
+player = Player()
+all_sprite.add(player)
+
 
 game = True
 while game:
@@ -104,25 +168,7 @@ while game:
         if e.type == pg.QUIT:
             game = False
 
-    '''
-    car1.y -= 1
-    if car1.y < -car1_h:
-        car1.y = HEIGHT
 
-    bg_image_rect.y += 1
-    if bg_image_rect.y > HEIGHT:
-        bg_image_rect.y = 0
-
-    bg_image2_rect.y += 1
-    if bg_image2_rect.y > 0:
-        bg_image2_rect.y = -HEIGHT
-
-    screen.fill(GREY)
-#bg()
-    screen.blit(bg_image, bg_image_rect)
-    screen.blit(bg_image, bg_image2_rect)
-    screen.blit(car1_image, (car1.x, car1.y))
-    '''
     all_sprite.update()
     all_sprite.draw(screen)
 
